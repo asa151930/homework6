145da88cb46fab9302e3d734575b041c606603b9
Ali Arab <aliarab812@yahoo.com>
2021-03-20 06:41:21 -0500

    added a folder and now making changes to the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // 564ecbbf68c5305bd1631046fcfc2982
 console.log("Hello World");
 
-$(document).ready(function(){   // document is the object
-    alert("JQuery is loaded DUDE!!")
+$(document).ready(function () {   // document is the object
+    //GIVEN a weather dashboard with form inputs
+    //WHEN I search for a city
+    //THEN I am presented with current and future conditions for that city and that city is added to the search history
+
 });
